c7d11667df6e264dc87d2be66411cd87d4551234
John Acosta <146490109+johnacosta23@users.noreply.github.com>
2024-04-17 15:35:26 +0200

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-var=$(grubby --info=ALL | grep title | awk '{print $5}' | tr -d '()' | head -1)
+var=$(grubby --info=ALL | awk '/^title/ {print $5}' | tr -d '()' | head -1)
 rm -rf /boot/initramfs-$var.img
 reboot
